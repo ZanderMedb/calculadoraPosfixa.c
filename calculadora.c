dc98639dc9c5f9c64e90dad25bae2561edec01d1
Zander Medeiros <128644514+ZanderMedb@users.noreply.github.com>
2024-06-17 15:12:40 -0300

    Update calculadora.c
    acerto de gramática

--- calculadora.c
+++ calculadora.c
@@ -89,14 +89,14 @@ int main() {
     char choice;
 
     do {
-        printf("Escreva a operação posfixa: ");
+        printf("Escreva a operacao posfixa: ");
         fgets(expression, MAX, stdin);
 
         expression[strcspn(expression, "\n")] = 0;
 
         printf("Resultado: %.2f\n", Posfixa(expression));
 
-        printf("Deseja fazer outra operação? (s/n) : ");
+        printf("Deseja fazer outra operacao? (s/n) : ");
         choice = getchar();
         getchar();
     } while (choice == 's' || choice == 'S');
