7fa1f7d0b49d569a9c0dc3c712a949c21458130c
Zander Medeiros <128644514+ZanderMedb@users.noreply.github.com>
2024-06-17 19:00:14 -0300

    Create calculadora.h

---
+++ calculadora.h
@@ -0,0 +1,21 @@
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
+
+#include <math.h>
+
+#define M_PI 3.14159
+#define MAX 100
+
+typedef struct {
+    double data[MAX];
+    int top;
+} Stack;
+
+void fazPilha(Stack *stack);
+int PilhaCheia(Stack *stack);
+int PilhaVazia(Stack *stack);
+void push(Stack *stack, double value);
+double pop(Stack *stack);
+double Posfixa(char* expression);
+
+#endif // CALCULADORA_H
